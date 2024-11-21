28e071bc1c9df12d5f12139ce2fe17c75e1028b6
Alejandra Valdivia <alexvcodes@gmail.com>
2024-11-21 11:17:16 -0800

    feat: connect to database

--- api/index.js
+++ api/index.js
@@ -1,13 +1,26 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello ALEJANDRA VALDIVIA!!!');
+app.get("/", (req, res) => {
+  res.send("Hello ALEJANDRA VALDIVIA!!!");
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
 
 export default app;
