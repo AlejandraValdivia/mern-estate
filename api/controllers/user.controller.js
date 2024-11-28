07fe4accc356159b578eeb981fd49e44bfa7aa5a
Alejandra Valdivia <alexvcodes@gmail.com>
2024-11-27 18:45:14 -0800

    feat: add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 export const test = (req, res) => {
     res.json({ message: "API route is working! Hello Alejandra Valdivia from the Test page" })
@@ -28,3 +29,15 @@ export const updateUser = async (req, res, next) => {
         next(error);
     }
 };
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You can only delete your own account!"));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie("access_token");
+        res.status(200).json("User has been deleted!");
+    } catch (error) {
+        next(error);
+    }
+};
+       
