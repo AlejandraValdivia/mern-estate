b42be88cc838b401d8713f166180e635ac6abf37
Alejandra Valdivia <alexvcodes@gmail.com>
2024-12-06 13:53:28 -0800

    feat: create search api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -9,5 +9,6 @@ router.post('/update/:id', verifyToken, updateUser);
 router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/listings/:id', verifyToken, getUserListings);
 router.get('/:id', verifyToken, getUser);
+
 export default router;
 
